80ed7f88ff11f9e190660ef4a1678fda0420878c
DrOppenheimer <kevin.p.keegan@gmail.com>
2012-03-24 13:46:47 -0500

    Update DRISEE_requirements_check.sh

--- DRISEE_requirements_check.sh
+++ DRISEE_requirements_check.sh
@@ -15,11 +15,19 @@ if [[ $my_perl =~ "perl" ]]; then
     echo "     perl status - perl is installed here: "$my_perl
     echo "     check to make sure that your version is 5.12.1 or later"
     echo "          (the path above should tell you the version number)"
+    echo
+    echo " * Please note that users are responsible for making sure that their use of required"
+    echo "software products is compliant with existing licenses and/or user agreements
+    echo
     my_count=1
 else
     echo "PROBLEM"
     echo "     perl status - perl is not installed"
     echo "          try here: http://www.perl.org/get.html"
+    echo
+    echo " * Please note that users are responsible for making sure that their use of required"
+    echo "software products is compliant with existing licenses and/or user agreements
+    echo
 fi
 echo
 
@@ -32,10 +40,18 @@ if [[ $my_python =~ "python" ]]; then
     echo "          (the path above should tell you the version number)"
     echo "     also make sure that you have biopython installed: http://biopython.org/wiki/Biopython"
     my_count=2
+    echo
+    echo " * Please note that users are responsible for making sure that their use of required"
+    echo "software products is compliant with existing licenses and/or user agreements
+    echo
 else
     echo "PROBLEM"
     echo "     python status - perl is not installed"
     echo "          try here: http://python.org/getit/"
+    echo
+    echo " * Please note that users are responsible for making sure that their use of required"
+    echo "software products is compliant with existing licenses and/or user agreements
+    echo
 fi
 echo
 
@@ -48,9 +64,12 @@ if [[ $my_uclust =~ "uclust" ]]; then
 else
     echo "PROBLEM"
     echo "     uclust status - uclust is not installed"
-    echo "          try here: http://www.drive5.com/uclust/downloads1_2_15.html"
-    echo "          or here:  http://www.drive5.com/usearch/nonprofit_form.html"
+    echo "          try here:  http://www.drive5.com/usearch/nonprofit_form.html"
     echo "          email:    robert@drive5.com"
+    echo
+    echo " * Please note that users are responsible for making sure that their use of required"
+    echo "software products is compliant with existing licenses and/or user agreements
+    echo
 fi
 echo
 
@@ -66,16 +85,28 @@ else
     echo "     cdbfasta status - cdbfasta is not installed"
     echo "          try here: http://sourceforge.net/projects/cdbfasta/"
     echo "          email: email: gpertea@tigr.org"
+    echo
+    echo " * Please note that users are responsible for making sure that their use of required"
+    echo "software products is compliant with existing licenses and/or user agreements
+    echo
 fi
 if [[ $my_cdbyank =~ "cdbyank" ]]; then
     echo " OK"
     echo "     cdbyank status - cdbyank is installed here: "$my_cdbyank
+    echo
+    echo " * Please note that users are responsible for making sure that their use of required"
+    echo "software products is compliant with existing licenses and/or user agreements
+    echo
     my_count=5
 else
     echo "PROBLEM"
     echo "     cdbyank status - cdbyank is not installed"
     echo "          try here: http://sourceforge.net/projects/cdbfasta/"
     echo "          email: email: gpertea@tigr.org"
+    echo
+    echo " * Please note that users are responsible for making sure that their use of required"
+    echo "software products is compliant with existing licenses and/or user agreements
+    echo
 fi
 
 echo
